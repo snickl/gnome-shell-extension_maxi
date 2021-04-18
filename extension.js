0e7aef9b60419a101c00c75727d08522a4b76fc8
Steven Mey <darkretailer@users.noreply.github.com>
2021-04-18 16:46:06 +0200

    Workaround for Wayland
    gtk_application_id is empty be new created windows in wayland:
    See https://extensions.gnome.org/errors/view/13258
    and https://gitlab.gnome.org/GNOME/mutter/-/issues/157

--- extension.js
+++ extension.js
@@ -4,33 +4,39 @@ var ExtensionUtils = imports.misc.extensionUtils;
 var _windowCreatedId;
 var _settings;
 
-
 function init() {
     _settings = ExtensionUtils.getSettings();
 }
 
 function enable() {
     _windowCreatedId = global.display.connect('window-created', (d, win) => {
-        const DEBUG = _settings.get_boolean('debug');
+        win._maxievtId = win.connect('focus', (win) => {
 
-        if (DEBUG) {
-            global.log("gnome-extension maxi@darkretailer.github.com: New " + win.gtk_application_id);
-        }
+            const DEBUG = _settings.get_boolean('debug');
 
-        if (!this._settings.get_strv('blacklisted-apps').includes(win.gtk_application_id + ".desktop")) {
-            if (win.can_maximize()) {
-                if (_settings.get_boolean('vertical')) {
-                    win.maximize(Meta.MaximizeFlags.VERTICAL);
-                }
-                if (_settings.get_boolean('horizontal')) {
-                    win.maximize(Meta.MaximizeFlags.HORIZONTAL);
-                }
-            }
-        } else {
             if (DEBUG) {
-                global.log('gnome-extension maxi@darkretailer.github.com: "' + win.gtk_application_id + '" is blacklisted');
+                global.log(" \
+                gnome-extension maxi@darkretailer.github.com: gtk_application_id: "
+                    + win.gtk_application_id
+                );
             }
-        }
+
+            if (!this._settings.get_strv('blacklisted-apps').includes(win.gtk_application_id + ".desktop")) {
+                if (win.can_maximize()) {
+                    if (_settings.get_boolean('vertical')) {
+                        win.maximize(Meta.MaximizeFlags.VERTICAL);
+                    }
+                    if (_settings.get_boolean('horizontal')) {
+                        win.maximize(Meta.MaximizeFlags.HORIZONTAL);
+                    }
+                }
+            } else {
+                if (DEBUG) {
+                    global.log('gnome-extension maxi@darkretailer.github.com: "' + win.gtk_application_id + '" is blacklisted');
+                }
+            }
+            win.disconnect(win._maxievtId);
+        });
     });
 }
 
